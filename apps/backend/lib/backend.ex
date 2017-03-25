b80ca693c3eef6399b6702cf27e4b80ca54e7dfc
Janis Miezitis <janjiss@gmail.com>
2017-03-25 12:59:11 -0600

    Fixed Backend.Repo supervision

--- apps/backend/lib/backend.ex
+++ apps/backend/lib/backend.ex
@@ -1,12 +1,2 @@
 defmodule Backend do
-  def start(_type, _args) do
-    import Supervisor.Spec, warn: false
-
-    children = [
-      supervisor(Backend.Repo, [])
-    ]
-
-    opts = [strategy: :one_for_one, name: Backend.Supervisor]
-    Supervisor.start_link(children, opts)
-  end
 end
